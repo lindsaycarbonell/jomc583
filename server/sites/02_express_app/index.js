6c822e664f0abf4e075c2134ad2f3563587195fd
Lindsay Carbonell <lindsaycarbonell@gmail.com>
2016-03-03 09:21:00 -0500

    express quiz added

--- server/sites/02_express_app/index.js
+++ server/sites/02_express_app/index.js
@@ -2,17 +2,41 @@
 var express = require('express');
 var app = express();
 
+//object array for testing ejs
+var data = {
+    lindsay: {
+      name: "Lindsay",
+      age: 20,
+      food: "rice"
+    },
+    andy: {
+      name: "Andy",
+      age: 21,
+      food: "COOOOOKIES"
+    }
+};
+
 app.set('view engine', 'ejs');
 
 //function to create a response when the user requests root
 app.get('/', function(req, res){
   //automatically looks in views
-  res.render('index');
+  res.render('list', data);
 });
 
-// app.get('/adele', function(req, res){
-//   res.send("HELLO.<br>IT'S ME.");
-// });
+app.get('/lindsay', function(req, res){
+  //automatically looks in views
+  res.render('index', data.lindsay);
+});
+
+app.get('/andy', function(req, res){
+  //automatically looks in views
+  res.render('index', data.andy);
+});
+
+app.get('/adele', function(req, res){
+  res.send("HELLO.<br>IT'S ME.");
+});
 
 app.listen(8080, function(){
   console.log("listening on 8080");
